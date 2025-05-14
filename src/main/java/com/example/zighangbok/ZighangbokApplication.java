32b5b9e2151660c66d7e2df4eff4aaafaae78dc7
jihyun <chiiclaro@gmail.com>
2025-05-14 18:20:06 +0900

    docs: 초기 세팅

---
+++ src/main/java/com/example/zighangbok/ZighangbokApplication.java
@@ -0,0 +1,13 @@
+package com.example.zighangbok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZighangbokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZighangbokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zighangbok/ZighangbokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zighangbok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZighangbokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
