343991afb905d878ad4564de44ba4e6e7ce00906
wosyh18 <wosyh18@naver.com>
2025-08-06 17:55:03 +0900

    역 직렬화를 위해 redis 기본 생성자 추가

--- src/main/java/com/backend/zighangbok/domain/recruitment/dto/RecruitmentSimpleDto.java
+++ src/main/java/com/backend/zighangbok/domain/recruitment/dto/RecruitmentSimpleDto.java
@@ -1,10 +1,14 @@
 package com.backend.zighangbok.domain.recruitment.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class RecruitmentSimpleDto {
     private String uuid;
     private String title;
